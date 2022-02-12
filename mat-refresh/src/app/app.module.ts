81990f3efdb849159b469014300b3ad85569e543
Bobby <bobby.pinard@gmail.com>
2022-02-12 14:53:21 -0600

    Date finished

--- mat-refresh/src/app/app.module.ts
+++ mat-refresh/src/app/app.module.ts
@@ -4,19 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Angular material imports
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
+//User generated components
 import { CardComponent } from './card/card.component';
 import { BadgesComponent } from './badges/badges.component';
+import { DateComponent } from './date/date.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    BadgesComponent
+    BadgesComponent,
+    DateComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +30,8 @@ import { BadgesComponent } from './badges/badges.component';
     MatCardModule,
     MatBadgeModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
     MatButtonModule
   ],
   providers: [],
